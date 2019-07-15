e19137db49fee8d468112c99e1144d410c1b383d
Alexander Smith <amsmith@uber.com>
2019-07-15 15:05:55 -0700

    Signature changes to include Action + ReactActions

--- src/index.flow.js
+++ src/index.flow.js
@@ -2,21 +2,22 @@
 
 import type {StoreCreator, Action, Reducer} from 'redux';
 
-type ReactorAction<S, T> = {|
+export type ReactorAction<S, T> = {|
   type: T,
-  payload?: mixed,
-  __REACTOR__?: Reducer<S, Action<T>>,
+  payload: mixed,
+  __REACTOR__: Reducer<S, ReactorAction<S, T>>,
 |};
 
 export function reactorEnhancer<
   S,
   ActionType,
-  A: ReactorAction<S, ActionType>,
+  A: Action<ActionType> | ReactorAction<S, ActionType>,
   D
 >(createStore: StoreCreator<S, A, D>): StoreCreator<S, A, D> {
   return function storeCreator(reducer, ...remainingArgs) {
     function wrappedReducer(state: S | void, action: A): S {
       if (action.__REACTOR__) {
+        // $FlowFixMe
         return action.__REACTOR__(state, action);
       }
       return reducer(state, action);
@@ -27,9 +28,9 @@ export function reactorEnhancer<
 
 export function createReactor<S, T>(
   type: T,
-  __REACTOR__: Reducer<S, Action<T>>
+  __REACTOR__: Reducer<S, ReactorAction<S, T>>
 ): (payload: mixed) => ReactorAction<S, T> {
-  return function actionCreator(payload: mixed) {
+  return function actionCreator(payload: mixed): ReactorAction<S, T> {
     return {
       type: type,
       payload: payload,
